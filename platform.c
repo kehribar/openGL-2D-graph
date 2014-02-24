546272a0891d9ea6d08aad36143800cb44a1f459
ihsan Kehribar <ihsan@kehribar.me>
2014-02-24 13:56:03 +0200

    Serial port opening part is added

--- platform.c
+++ platform.c
@@ -8,28 +8,47 @@
 /----------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdint.h>
-#include <math.h>
 
-#define PI 3.14159265358979323846
+int n;
+int port;
+int flags;
+unsigned char buf[16384];
+struct termios settings;
 
 void fillScreenBuffer(uint16_t* screenBufferPointer,uint16_t size,uint16_t topValue)
 {
-	for (int i = 0; i < size; ++i)
-	{
-		// synthetic sinus signal
-		screenBufferPointer[i] = ((double)(topValue/2) * sin(2*PI*((double)i/(double)size))) + topValue/2;
-	}
+
 }
 
 // return 0 for successful hardware/software platform initialization
 int platform_init(int argc, char** argv)
 {
-	/*
+	
 	for (int i = 0; i < argc; ++i)
 	{
 		printf("[%d] %s\n",i,argv[i]);
 	}
-	*/
+		
+	if (argc < 2) 
+	{
+		printf("Example: serial_listen [port_path]\n");
+		return -1;
+	}
+
+	// Open the serial port
+	port = open(argv[1], O_RDWR);
+	if (port < 0) 
+	{
+		printf("Unable to open %s\n", argv[1]);
+		return -1;
+	}
+
+	// Configure the port
+	flags = fcntl(port, F_GETFL, 0);
+	fcntl(port, F_SETFL, flags | O_NONBLOCK);
+	tcgetattr(port, &settings);
+	cfmakeraw(&settings);
+	tcsetattr(port, TCSANOW, &settings);
 
 	return 0;
 }
